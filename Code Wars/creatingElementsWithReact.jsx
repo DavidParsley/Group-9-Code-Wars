53fd7ac33946dbb9821ae9ed19f9090456f10f77
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-11-01 18:23:18 +0300

    Completed Code war

---
+++ Code Wars/creatingElementsWithReact.jsx
@@ -0,0 +1,15 @@
+var React = require("react");
+
+function createElement(content, tag="div", props={}) {
+  // create a react element here
+ return React.createElement(
+     tag,
+     props ,
+    'Hello World'
+  );
+}
+
+function createUnorderedList(list) {
+  const listItems = list.map(item => React.createElement('li', { key: item }, item = "Hello World"));
+  return React.createElement('ul', null, ...listItems);
+}
---
+++ Code Wars/reactWithJSX.jsx
@@ -0,0 +1,27 @@
+let React = require('react')
+
+// let us know who your three favorite codewarriors are!
+// mantain this format, but choose any three users you want:
+// <h1>My three favorite codewarriors are noLan, jhoffner and OverZealous</h1>
+var myHeader = <h1>My three favorite codewarriors are noLan, jhoffner and OverZealous</h1>
+
+
+var CodewarsLink = React.createClass({
+  render: function() {
+    return (
+      <a href={`http://www.codewars.com/users/${this.props.user}`}>{this.props.user}</a>
+    );
+  }
+});
+
+var Leaderboard = React.createClass({
+  render: function() {
+    return (
+      <div>
+        {this.props.leaders.map(function(leader) {
+          return <CodewarsLink user={leader} key={leader} />;
+        })}
+      </div>
+    );
+  }
+});
